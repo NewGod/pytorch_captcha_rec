1da184ac064d5e2038808798d64732acc91b5e95
liangxi11 <1600012774@pku.edu.cn>
2017-12-09 21:54:44 +0800

    softmax needs modifying

--- model/test1.py
+++ model/test1.py
@@ -6,7 +6,8 @@ SIZE2 = 18
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.pool  = nn.MaxPool2d(2,2)
+        self.softmax = nn.LogSoftmax(dim = 0)
+        self.pool   = nn.MaxPool2d(2,2)
         self.conv11 = nn.Conv2d(3, SIZE1, 3, padding = 1) # 1 input image channel, 64 output channels, 5x5 square convolution kernel
         self.conv12 = nn.Conv2d(SIZE1, SIZE1, 3, padding = 1);
         
@@ -37,7 +38,8 @@ class Net(nn.Module):
         x = F.relu(self.fc2(x))
         x = F.relu(self.fc3(x))
         x = self.fc4(x)
-        x = F.softmax(x)
         x = x.view(-1,4,36)
+        x = self.softmax(x)
+        print(x)
         return x
 
